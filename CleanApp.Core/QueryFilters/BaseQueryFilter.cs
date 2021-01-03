f4dcb8467ef715ea5db4464e0aa27d05960b7fb5
Raul Fernandez Fernandez <raulparri71@gmail.com>
2021-01-03 10:28:21 +0100

    mejoras

--- CleanApp.Core/QueryFilters/BaseQueryFilter.cs
+++ CleanApp.Core/QueryFilters/BaseQueryFilter.cs
@@ -2,7 +2,14 @@
 {
     public abstract class BaseQueryFilter
     {
+        /// <summary>
+        /// Tamaño de la paginación
+        /// </summary>
         public int PageSize { get; set; }
+
+        /// <summary>
+        /// Número de la página
+        /// </summary>
         public int PageNumber { get; set; }
     }
 }
--- CleanApp.Core/QueryFilters/HomeQueryFilter.cs
+++ CleanApp.Core/QueryFilters/HomeQueryFilter.cs
@@ -2,9 +2,24 @@
 {
     public class HomeQueryFilter : BaseQueryFilter
     {
+        /// <summary>
+        /// Dirección de la vivienda
+        /// </summary>
         public string HomeAddress { get; set; }
+
+        /// <summary>
+        /// Ciudad de la vivienda
+        /// </summary>
         public string HomeCity { get; set; }
+
+        /// <summary>
+        /// País de la vivienda
+        /// </summary>
         public string HomeCountry { get; set; }
+
+        /// <summary>
+        /// Código postal de la vivienda
+        /// </summary>
         public string HomeZipCode { get; set; }
     }
 }
